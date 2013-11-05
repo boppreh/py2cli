aaf1f318f1b451d65255126c369ac07345569549
BoppreH <boppreh@gmail.com>
2013-11-05 20:33:26 -0200

    Add docs to  class

--- py2cli.py
+++ py2cli.py
@@ -3,7 +3,14 @@ import re
 from inspect import signature, Signature, _empty
 
 class Args(object):
+    """
+    Class for storing arguments provided by the user and interactively calling
+    functions with them.
+    """
     def __init__(self, argv):
+        """
+        Creates a new Args instance from a list of command line arguments.
+        """
         self.args = []
         self.kwargs = {}
 
@@ -22,6 +29,10 @@ class Args(object):
         self.official_args = {}
 
     def request(self, name, description='', default=_empty):
+        """
+        Returns a specific argument, interactively prompting the user for items
+        if it doesn't exist.
+        """
         if name in self.kwargs:
             return self.kwargs[name]
         elif name[0] in self.kwargs:
@@ -29,28 +40,49 @@ class Args(object):
         elif len(self.args):
             return self.args.pop(0)
         else:
-            name = name.replace('_', ' ').title()
-            if description:
-                prompt = '{} ({}): '.format(name, description)
-            else:
-                prompt = '{}: '.format(name)
+            return self.request_interactive(name, description, default)
 
-            if default is _empty:
-                return input(prompt)
-            else:
-                result = input(prompt + ' (default: {}) '.format(default))
-                if result == '':
-                    return default
-                else:
-                    try:
-                        return eval(result)
-                    except (SyntaxError, NameError):
-                        return result
+    def request_interactive(self, name, description='', default=_empty):
+        """
+        Requests a value by prompting the user.
+        """
+        name = name.replace('_', ' ').title()
+        if description:
+            prompt = '{} ({}): '.format(name, description)
+        else:
+            prompt = '{}: '.format(name)
+
+        if default is _empty:
+            return input(prompt)
+        else:
+            return self._prompt_for_value(prompt, default)
+
+    def _prompt_for_value(self, prompt, default=_empty):
+        """
+        Displays a prompt to the user and returns the evaluated result,
+        possibly returning an empty value if the user didn't type anything.
+        """
+        result = input(prompt + '(default: {}) '.format(default))
+        if result == '':
+            return default
+        else:
+            try:
+                return eval(result)
+            except (SyntaxError, NameError):
+                return result
 
     def ensure_exists(self, name, description='', default=_empty):
+        """
+        Ensures a given argument exists, possibly prompting the user to enter 
+        it.
+        """
         self.official_args[name] = self.request(name, description, default)
 
     def apply(self, function):
+        """
+        Apply the arguments that have been ensured to exist to a given
+        function.
+        """
         return function(**self.official_args)
 
 if __name__ == '__main__':
